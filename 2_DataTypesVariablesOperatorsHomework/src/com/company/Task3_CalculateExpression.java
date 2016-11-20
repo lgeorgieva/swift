02c38e48d6bd54f33e1bca018314ebbfd8e98845
Lyubomira Georgieva <luba.georgieva@gmail.com>
2016-11-20 11:57:26 +0200

    i
    l

---
+++ 2_DataTypesVariablesOperatorsHomework/src/com/company/Task3_CalculateExpression.java
@@ -0,0 +1,16 @@
+package com.company;
+
+/**
+ * Created by ASUS on 21.10.2016 г..
+ */
+public class Task3_CalculateExpression {
+    public static void main(String[] args) {
+        double a = ( 3291 + 88581 ) / 14;
+        double b =  ( 1116 % 171 ) * 5;
+        double c =  312 / ( 4 + 18 );
+        double result = a + b - c;
+
+        System.out.println(result);
+
+    }
+}
